e0f501be4c77fdef4a5fbb599e69c703b3b46aca
liudingyu <derek.liu0221@gmail.com>
2017-02-27 16:42:55 +0800

    add UriMatcher test

--- app/src/main/java/com/example/liudingyu/examplegradle/activity/MainActivity.java
+++ app/src/main/java/com/example/liudingyu/examplegradle/activity/MainActivity.java
@@ -1,28 +1,36 @@
 package com.example.liudingyu.examplegradle.activity;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.content.UriMatcher;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 import com.example.liudingyu.examplegradle.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private String[] mData = new String[]{"What is design?", "Design", "Design is not just", "what it looks like", "and feels like.", "Design", "is how it works.", "- Steve Jobs", "Older people", "sit down and ask,", "'What is it?'", "but the boy asks,", "'What can I do with it?'.", "- Steve Jobs", "Swift", "Objective-C", "iPhone", "iPad", "Mac Mini", "MacBook Pro", "Mac Pro", "爱老婆", "老婆和女儿"};
     private ImageView mImageView, mSecondImageView;
+
+    private static final String AUTHORITY = "com.derek.myapp";
+    private static Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    private static String PATH_PEOPLE = "people";
+    private static String PATH_PEOPLE_NUMBER = "people_number";
+
+    private static UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    private static final int PATH_PEOPLE_ID = 0;
+    private static final int PATH_PEOPLE_NUMBER_ID = 1;
+
+    static {
+        uriMatcher.addURI(AUTHORITY, PATH_PEOPLE, PATH_PEOPLE_ID);
+        uriMatcher.addURI(AUTHORITY, PATH_PEOPLE_NUMBER, PATH_PEOPLE_NUMBER_ID);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,45 +39,15 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.go).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                handleClick(mImageView);
-//                ((BitmapDrawable) mImageView.getDrawable()).getBitmap().recycle();
-//                findViewById(R.id.root_view).invalidate();
-//                mImageView.requestLayout();
-//                mSecondImageView.requestLayout();
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+                handleClick(v);
             }
         });
-        /*
-        mImageView = (ImageView) findViewById(R.id.firstImage);
-        mSecondImageView = (ImageView) findViewById(R.id.secondImage);
-        mSecondImageView.setImageDrawable(mImageView.getDrawable());
-        */
     }
 
-    private void handleClick(View view) {
-        Intent intent = new Intent(this, VideoActivity.class);
-        startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(this, view, "sharedView").toBundle());
+    private void handleClick(View v) {
+        Uri uri = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PEOPLE_NUMBER).build();
+        int id = uriMatcher.match(uri);
+        Log.d("d.d", "id: " + id);
     }
 
-    private void handleNotification() {
-        String bigText = "bigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigTextbigText";
-        Drawable drawable = getResources().getDrawable(R.drawable.smoke);
-        Bitmap icon = null;
-        if (drawable instanceof BitmapDrawable) {
-            icon = ((BitmapDrawable) drawable).getBitmap();
-        }
-        NotificationManager mNm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setTicker("Ticker").setSmallIcon(R.drawable.fire).setAutoCancel(true);
-        builder.setContentTitle("contentTitle").setContentText("contentText").setLargeIcon(icon).setWhen(System.currentTimeMillis());
-        builder.setDefaults(Notification.DEFAULT_ALL);
-        builder.setStyle(new NotificationCompat.BigTextStyle().bigText(bigText));
-
-        mNm.notify(1, builder.build());
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-    }
 }
